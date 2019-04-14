099d5de3896d8d9fa39bb4d63ce60331762d3ead
Robert Middleton <robert.middleton@rm5248.com>
2019-04-14 13:09:14 -0400

    Create new static pc file

--- cppgenerate-static.pc.cmake
+++ cppgenerate-static.pc.cmake
@@ -1,9 +1,9 @@
-Name: cppgenerate
+Name: cppgenerate-static
 Description: A library for generating C++ code from code
 Version: ${PROJECT_VERSION}
 Requires: ${PKG_CONFIG_REQUIRES}
 prefix=${CMAKE_INSTALL_PREFIX}
 includedir=${PKG_CONFIG_INCLUDEDIR}
 libdir=${PKG_CONFIG_LIBDIR}
-Libs: ${PKG_CONFIG_LIBS}
+Libs: ${PKG_CONFIG_STATIC_LIBS}
 Cflags: ${PKG_CONFIG_CFLAGS}
